0caf5b9e1a8af06ecfdd42f3b68f4d44a772d0ed
IlyaPolo <ilya.poloshovets@gmail.com>
2020-10-24 16:10:15 +0300

    Второе задание

---
+++ hm2/src/main/java/ru/javacourse/hm2.java
@@ -0,0 +1,21 @@
+package ru.javacourse;
+
+public class hm2 {
+    public static void main(String[] args) {
+        byte a = 127;
+        short b = 32767;
+        char c = 'c';
+        int d = 2147483647;
+        long e = 9223372036854775807L;
+        float f = 3.1f;
+        double g = 3.14;
+        String h = "Hello World!";
+        boolean tr = true; // или false;
+
+
+
+    }
+
+
+
+}
